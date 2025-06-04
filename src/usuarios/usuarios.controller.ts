f7c10f1fb816ea8e5915fb6ad9ce8cd10c2db8d8
MiltonGMZ <milframurgo16@gmail.com>
2025-06-03 22:19:38 -0500

    save

--- src/usuarios/usuarios.controller.ts
+++ src/usuarios/usuarios.controller.ts
@@ -11,7 +11,10 @@ export class UsuariosController {
 
   @Get()
   @ApiOperation({ summary: 'Obtener todos los usuarios' })
-  @ApiResponse({ status: 200, description: 'Lista de usuarios obtenida correctamente' })
+  @ApiResponse({
+    status: 200,
+    description: 'Lista de usuarios obtenida correctamente',
+  })
   getAllUsuarios() {
     return this.usuariosService.getAllUsuarios();
   }
@@ -33,34 +36,33 @@ export class UsuariosController {
   }
 
   @Put(':uid')
-@ApiOperation({ summary: 'Actualizar usuario existente' })
-@ApiParam({ name: 'uid', description: 'UID del usuario a actualizar' })
-@ApiResponse({ status: 200, description: 'Usuario actualizado exitosamente' })
-updateUsuario(@Param('uid') uid: string, @Body() body: UpdateUsuarioDto) {
-  return this.usuariosService.updateUsuarios([uid], body);
-}
+  @ApiOperation({ summary: 'Actualizar usuario existente' })
+  @ApiParam({ name: 'uid', description: 'UID del usuario a actualizar' })
+  @ApiResponse({ status: 200, description: 'Usuario actualizado exitosamente' })
+  updateUsuario(@Param('uid') uid: string, @Body() body: UpdateUsuarioDto) {
+    return this.usuariosService.updateUsuarios([uid], body);
+  }
 
   @Delete(':uid')
   @ApiOperation({ summary: 'Eliminar usuario' })
   @ApiParam({ name: 'uid', description: 'UID del usuario a eliminar' })
   @ApiResponse({ status: 200, description: 'Usuario eliminado' })
   deleteUsuario(@Param('uid') uid: string) {
-    console.log('UID Recibido:', uid);
     return this.usuariosService.deleteUsuario(uid);
   }
 
   @Get('abogados')
-@ApiOperation({ summary: 'Obtener todos los abogados' })
-@ApiResponse({ status: 200, description: 'Abogados obtenidos correctamente' })
-@ApiResponse({ status: 404, description: 'No se encontraron abogados' })
-async getAbogados() {
-  try {
-    return await this.usuariosService.getAbogados();
-  } catch (error) {
-    throw new HttpException('No se encontraron abogados', HttpStatus.NOT_FOUND);
+  @ApiOperation({ summary: 'Obtener todos los abogados' })
+  @ApiResponse({ status: 200, description: 'Abogados obtenidos correctamente' })
+  @ApiResponse({ status: 404, description: 'No se encontraron abogados' })
+  async getAbogados() {
+    try {
+      return await this.usuariosService.getAbogados();
+    } catch (error) {
+      throw new HttpException(
+        'No se encontraron abogados',
+        HttpStatus.NOT_FOUND,
+      );
+    }
   }
-}
-
-
-
 }
